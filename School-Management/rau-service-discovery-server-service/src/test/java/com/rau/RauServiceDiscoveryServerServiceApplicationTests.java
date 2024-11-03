2a8a0cda79db8de9cb66f75da8b0f6a910cf8acd
Raushan1234567 <raushankumar241099@gmail.com>
2024-11-03 07:40:21 +0530

    register service discovery and also register a service as a client

---
+++ School-Management/rau-service-discovery-server-service/src/test/java/com/rau/RauServiceDiscoveryServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RauServiceDiscoveryServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ School-Management/rau-student-service/src/main/java/com/rau/RauStudentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RauStudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RauStudentServiceApplication.class, args);
+	}
+
+}
